73fa62bbda02bfdb7a6c23cc2be0045348a712ca
RadLea <radu.leanca@softwire.com>
2023-06-28 14:35:50 +0300

    code consistency

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,7 +3,7 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('Normal Item', function() {
+    it('Normal Item', () => {
         const gildedRose = new GildedRose([ new Item('burger', 25, 15) ]);
         const items = gildedRose.updateQuality();
         expect(items[0]).deep.equal(new Item('burger', 24, 14));
@@ -56,6 +56,5 @@ describe('Gilded Rose', function () {
         const items = gildedRose.updateQuality();
         expect(items[0]).deep.equal(new Item('Aged Brie', -3, 17));
     });
-
 
 });
